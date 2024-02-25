0175e50b9eeedf8bbad7eebce6fcdcdf81989c46
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-25 13:49:02 -0600

    hashset implementation

--- src/util/map_and_set.rs
+++ src/util/map_and_set.rs
@@ -1,14 +1,24 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn test_map_and_set() {
-   let mut my_map = HashMap::<String, f32>::new(); 
-   my_map.insert("pi".to_string(), 3.14159);
-   my_map.insert("e".to_string(), 2.71828);
-    my_map.insert("pi".to_string(), 3.14159);
-   my_map.insert("e".to_string(), 2.71828);
-   // println!("my_map: {:#?}", my_map);
 
-   for (key, value) in my_map {
-      println!("{}: {}", key, value);
+   let mut my_set = HashSet::<String>::new();
+   my_set.insert("pi".to_string());
+   my_set.insert("pi".to_string());
+   my_set.insert("e".to_string());
+   
+   for value in my_set {
+      println!("{}", value);
    }
+
+   // let mut my_map = HashMap::<String, f32>::new(); 
+   // my_map.insert("pi".to_string(), 3.14159);
+   // my_map.insert("e".to_string(), 2.71828);
+   //  my_map.insert("pi".to_string(), 3.14159);
+   // my_map.insert("e".to_string(), 2.71828);
+   // // println!("my_map: {:#?}", my_map);
+
+   // for (key, value) in my_map {
+   //    println!("{}: {}", key, value);
+   // }
 }
