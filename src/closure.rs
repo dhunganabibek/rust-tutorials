99715d2eeaff3673b7f3d96b684d2897837f6baf
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-24 19:48:39 -0600

    closure  modifying external scope variable also should me mutable

--- src/closure.rs
+++ src/closure.rs
@@ -1,5 +1,24 @@
+struct Person {
+    first: String,
+    last: String,
+}
+
+
 pub fn test_closures(){
     let add = |x, y| x + y; 
     let result = add(20, 30);
     println!("The result is: {}", result);
+
+    let mut  person = Person{
+        first: "Bibek".to_string(),
+        last: "Dhungana".to_string()
+    };
+
+    let mut modify_last_name = || {
+        person.last = "Shrestha".to_string();
+    };
+
+    modify_last_name();
+
+    println!("lastname: {}", person.last);
 }
