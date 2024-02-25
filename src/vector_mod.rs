b9aabf902c2e0342ae7b97515333a956a8709bed
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-25 13:00:21 -0600

    created vector of custom struct

--- src/vector_mod.rs
+++ src/vector_mod.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 pub fn test_vector(){
     let mut  v: Vec<i32> = Vec::new();
 
@@ -8,4 +10,28 @@ pub fn test_vector(){
     println!("{:?}",v.len());
     println!("{:?}",v.capacity());
     println!("{:?}",&(&v).as_slice()[0..5]);
+    println!("{:?}",v.get(1).unwrap());
+
+    let names = vec!["Bibek", "Dhungana", "Hello"];
+    for name in names.clone(){
+        println!("{}",name);
+    }
+    println!("{:?}",names);
+
+    let mut cars: Vec<Car> = Vec::new();
+    cars.push(Car{
+        manufacturer: String::from("Toyota"),
+        model: String::from("Corolla"),
+    });
+    cars.push(Car{
+        manufacturer: String::from("Honda"),
+        model: String::from("Civic"),
+    });
+    println!("{:?}",cars);
+}
+
+#[derive(Debug)]
+struct Car{
+    manufacturer: String,
+    model: String,
 }
