2bb89a73523c152dcb83a58ba7aa94048d1d8254
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-25 12:30:47 -0600

    generic and traits in Rust

--- src/generic_mod.rs
+++ src/generic_mod.rs
@@ -1,3 +1,23 @@
-pub fn test_mod(){
-    println!("test mod");
+
+trait Animal{
+
 }
+struct Dog{
+
+}
+impl Animal for Dog{}
+struct Cat{}
+
+
+struct Person<PetType: Animal>{
+    first_name:String,
+    pet:PetType
+}
+
+pub fn test_mod(){
+    let p1 = Person{
+        first_name:String::from("John"),
+        pet: Dog{}
+    };
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-pub mod util;
-pub mod closure;
-pub mod match_demo;
-pub mod enum_demo;
-pub mod struct_mod;
 pub mod generic_mod;
 
 use generic_mod::test_mod;
