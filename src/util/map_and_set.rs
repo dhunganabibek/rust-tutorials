7f06b204caf4406012003abbdd2c422d46237f8d
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-25 13:41:43 -0600

    basic haspmap demo

--- src/util/map_and_set.rs
+++ src/util/map_and_set.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
+
 pub fn test_map_and_set() {
-   println!("test_map_and_set");
+   let mut my_map = HashMap::<String, f32>::new(); 
+   my_map.insert("pi".to_string(), 3.14159);
+   my_map.insert("e".to_string(), 2.71828);
+    my_map.insert("pi".to_string(), 3.14159);
+   my_map.insert("e".to_string(), 2.71828);
+   println!("my_map: {:#?}", my_map);
 }
