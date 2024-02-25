83404c22230f9242dd4c025d14a2648dc926eda1
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-25 13:47:22 -0600

    iterating through the map

--- src/util/map_and_set.rs
+++ src/util/map_and_set.rs
@@ -6,5 +6,9 @@ pub fn test_map_and_set() {
    my_map.insert("e".to_string(), 2.71828);
     my_map.insert("pi".to_string(), 3.14159);
    my_map.insert("e".to_string(), 2.71828);
-   println!("my_map: {:#?}", my_map);
+   // println!("my_map: {:#?}", my_map);
+
+   for (key, value) in my_map {
+      println!("{}: {}", key, value);
+   }
 }
