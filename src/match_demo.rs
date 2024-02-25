209bb1383c987b9303b84d57bc1a0fd9466d23f2
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-24 21:20:11 -0600

    is match guard useful

--- src/match_demo.rs
+++ src/match_demo.rs
@@ -1,12 +1,10 @@
 pub fn test_match(){
-    let my_age = 35;
+    let my_age: u8 = 36;
+
 
     match my_age {
-        35 => {
-            println!("You are 35 years old");
-        },
-        _ => {
-            println!("You are not 35 years old");
-        }
+        35 => println!("You are 35 years old"),
+        _ => println!("You are not 35 years old")
+        
     }
 }
