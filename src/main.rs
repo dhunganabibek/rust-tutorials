749cfab51c14ff7d9ac39541ce98552d51930120
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-24 18:39:00 -0600

    created externa; module and imported it the file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
+pub mod util;
+
+use util::{get_full_name, add};
+
+
 fn main() {
-    let a = 5;
-    let pi = 3.9415;
+    let full_name = get_full_name("John", "Doe");
+    println!("Full Name: {}", full_name);
 
-    let ans = a + pi as i32;
-
-    let my_str  = 'A';
-    let my_str2 = "Hello World!";
-
-    let my_tup = ("Hello", 5, 3.1415);
-    let ages:[u128; 5] = [1,2,3,4,5];
-
-    //slices
-    let slice = &ages[1..3];
-
-
-    
-    println!("{:?}", slice);
+    let sum = add(10, 20);
+    println!("Sum: {}", sum);
 }
+
---
+++ src/util.rs
@@ -0,0 +1,10 @@
+
+pub fn get_full_name (first: &str, last: &str) -> String{
+    let full_name = format!("{0} {1}", first, last);
+    return full_name;
+}
+
+
+pub fn add(a: i32, b:i32) -> i32{
+    return  a + b;
+}
