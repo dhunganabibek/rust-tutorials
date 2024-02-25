77601d1247a87f312d461ef4e4545ad6d9c4c2a0
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-24 19:39:18 -0600

    working with argument and  return type to closure

--- src/closure.rs
+++ src/closure.rs
@@ -1,4 +1,5 @@
 pub fn test_closures(){
-    let add = |x| println!("Testing closures: {}", x);
-    add(20);
+    let add = |x, y| x + y; 
+    let result = add(20, 30);
+    println!("The result is: {}", result);
 }
