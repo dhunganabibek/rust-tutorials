183d2b0a3556948d7b4056fc64e0d4391f97fd37
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-24 18:07:51 -0600

    playing with data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 5;
+    let pi = 3.9415;
+
+    let ans = a + pi as i32;
+
+    let myStr  = 'A';
+    let myStr2 = "Hello World!";
+
+    
+    println!("{}", ans);
 }
