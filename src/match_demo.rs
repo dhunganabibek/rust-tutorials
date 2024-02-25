c1a4e1fe7df7ea5fed8a1a762f81e13a014a1213
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-24 21:05:48 -0600

    match demo and used multiple arms

--- src/match_demo.rs
+++ src/match_demo.rs
@@ -1,3 +1,12 @@
 pub fn test_match(){
-    println!("Testing match");
+    let my_age = 35;
+
+    match my_age {
+        35 => {
+            println!("You are 35 years old");
+        },
+        _ => {
+            println!("You are not 35 years old");
+        }
+    }
 }
