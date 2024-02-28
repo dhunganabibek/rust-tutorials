424a3bb5626d14419135d0926d96b0734a1d16aa
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-28 17:53:42 -0600

    Hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-pub mod util;
 
-use util::serde_mod::test_serde;
+
 fn main() {
-  test_serde();
+  println!("Hello, world!")
 }
 
--- src/util/serde_mod.rs
+++ src/util/serde_mod.rs
@@ -1,3 +1,22 @@
+use serde::{Serialize, Deserialize};
+use serde_json::to_string;
+
 pub fn test_serde(){
-    println!("test_serde")
+    let dof = Dog {
+        name: "Rusty".to_string(),
+        year_born: 2018
+    };
+    let dog_json = to_string(&dof);
+
+    if (dog_json.is_ok()){
+        println!("Dog JSON: {}", dog_json.unwrap());
+    } else {
+        println!("Error: {}", dog_json.err().unwrap());
+    }
 } 
+
+#[derive(Serialize, Deserialize)]
+struct Dog {
+    name: String,
+    year_born: i32
+}
