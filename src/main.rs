da5e87fc891819a9fedfc1ba12d017b9b5d843ea
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-28 18:05:31 -0600

    Simple api created

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use axum::{routing::get, Router};
 
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
+  // initialize tracing
+  tracing_subscriber::fmt::init();
+
+  let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+  axum::serve(listener, app).await.unwrap();
   println!("Hello, world!")
 }
 
