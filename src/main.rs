0072c1df0b51176f71b112f50ad19e3626cf2593
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-28 19:04:32 -0600

    added data base url from .env file and read it using dotenv file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{routing::get, Json, Router};
 use sqlx::mysql::MySqlPoolOptions;
+use dotenv::dotenv;
 
 
 #[tokio::main]
@@ -7,16 +8,18 @@ async fn main() {
   // initialize tracing
   tracing_subscriber::fmt::init();
 
+  // loading from dotenv file
+  dotenv().ok();
+
   let pool = MySqlPoolOptions::new()
     .max_connections(5)
-    .connect("mysql://root:RisinG123%23@localhost:3306/test")
+    .connect(std::env::var("MYSQL_URL").unwrap().as_str())
     .await.unwrap();
   
   let row: (i64,) = sqlx::query_as("SELECT ?")
     .bind(42_i64)
     .fetch_one(&pool)
-    .await
-    .unwrap();
+    .await.unwrap();
 
   dbg!(row);
 
