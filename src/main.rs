af5b69a02b8afc3616fb4020aa67ab8cad5c779d
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-28 18:50:44 -0600

    sucessfully connected to database

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use axum::{routing::{get,post}, Json, Router};
-
+use axum::{routing::get, Json, Router};
+use sqlx::mysql::MySqlPoolOptions;
 
 
 #[tokio::main]
 async fn main() {
   // initialize tracing
   tracing_subscriber::fmt::init();
+
+  let pool = MySqlPoolOptions::new()
+    .max_connections(5)
+    .connect("mysql://root:RisinG123%23@localhost:3306/test")
+    .await.unwrap();
+  
+  let row: (i64,) = sqlx::query_as("SELECT ?")
+    .bind(42_i64)
+    .fetch_one(&pool)
+    .await
+    .unwrap();
+
+  dbg!(row);
 
   let app = Router::new()
     .route("/", get(|| async { "Hello, world!" }))
