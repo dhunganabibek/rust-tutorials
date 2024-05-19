ea2030c68377714d966d24eda9d7eef710b1ee9e
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-19 13:26:49 -0500

    working with borrow checker and life time

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
+use std::fmt::Debug;
 
-fn main(){
+
+fn main() -> (){
+  #[derive(Debug)]
+  struct FullName {
+    first_name: String,
+    last_name: String
+  };
+
+  let full_name = FullName{
+    first_name: String::from("Bibek"),
+    last_name: String::from("Dhungana")
+  };
+  
+  println!("{:?}", full_name);
+
+  let point: (i8, i8) = (3, 5);
+  let (x,y) = point;
   let greeting = "Hello, World!";
   let mut name = String::from("Bibek Dhungana");
   name = String::from("Bibek");
   let result = format!("{} {}", greeting, name);
   print!("{}", result);
+  let multiply_result = multiply(10, 20);
+  println!("{}", multiply_result);
+}
+
+fn multiply(a: i32, b: i32) -> i32{
+  return a * b;
 }
